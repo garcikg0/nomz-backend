f8e509fc5236215cb2866bdd41e30a934ce83dd2
garcikg0 <kg.gar91@gmail.com>
2021-02-17 17:35:31 -0500

    Kitchen CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   post "/login", to: "users#login"
   get "/autologin", to: "users#autologin"
+  delete "/kitchens/:id", to: "kitchens#destroy"
+  patch "/kitchens/:id", to: "kitchens#update"
+  post "/kitchens", to: "kitchens#create"
+  get "/kitchenuser", to: "kitchens#kitchen_user"
   get "/users", to: "users#index"
   get "/kitchens", to: "kitchens#index"
   get "/kitchens/:id", to: "kitchens#show"
-  delete "/kitchens/:id", to: "kitchens#destroy"
-  patch "/kitchens/:id", to: "kitchens#update"
-  get "/kitchenuser", to: "kitchens#kitchen_user"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
